b8d65ca4bec29ee5f33ba532e17ba3de55b1e666
Anton Pakhorukov <pakhorukov.a@mail.ru>
2022-09-15 00:51:52 +0700

    Example006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 1;
+int b = 8;
+int c = 3;
+int d = 2;
+int e = 6;
+int max = a;
+if (a > max) {
+    max = a;
+} else if (b > max) {
+    max = b;
+} else if (c > max) {
+    max = c;
+} else if (d > max) {
+    max = d;
+} else if (e > max) {
+    max = e;
+}
+Console.WriteLine(max);
