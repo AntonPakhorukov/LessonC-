c7b3dbd89b12d604294bc3ef7366f9c384b1670f
Anton Pakhorukov <pakhorukov.a@mail.ru>
2022-09-15 00:43:26 +0700

    Exemple005 Name

---
+++ Example005_Name/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите имя: ");
+String name = Console.ReadLine();
+//Console.WriteLine(name);
+if (name.ToLower() == "anton") {
+    Console.WriteLine("Ух ты! Это же Антон!");
+}
+else { 
+    Console.WriteLine("Привет " + name);
+}
