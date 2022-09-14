666f2cba959ac87f5028436f421e310dc3c2fd5c
Anton Pakhorukov <pakhorukov.a@mail.ru>
2022-09-15 00:15:34 +0700

    Exemple004_Div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿double numberA = new Random().Next(20, 30);
+double numberB = new Random().Next(1, 10);
+double div = numberA / numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(div);
